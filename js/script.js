8c9e53e215c0866f2b46b37257a7e9015a7ca71d
Benito <bbxblack@gmail.com>
2021-03-26 16:37:11 +0100

    v0.1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+// Creare un array di oggetti: ogni oggetto descriverà
+// una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+function racingBikes() {
+  let bikes = [
+    {
+      'name': 'ducati',
+      'weigth': 11
+    },
+    {
+      'name': 'ktm',
+      'weigth': 12
+    },
+    {
+      'name': 'maserati',
+      'weigth': 10
+    },
+    {
+      'name': 'ferrari',
+      'weigth': 14
+    },
+  ]
+}
+
+
+function init() {
+
+
+
+}
+
+$(document).ready(init);
